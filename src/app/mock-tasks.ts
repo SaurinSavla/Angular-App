b6f6b9cc711ea2dcf572dc811a7ce4f5c656801c
SaurinSavla <saurin.savla@gmail.com>
2023-06-22 00:12:37 +0530

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task'
+ 
+export const Tasks: Task[] = [
+	{
+		id: 1,
+		text: 'Doctor\'s Appointment',
+		day: 'June 25th at 2:30pm',
+		reminder: true
+	},
+	{
+		id: 2,
+		text: 'Bike Ride',
+		day: 'June 21st at 5.30am',
+		reminder: true
+	},
+	{
+		id: 3,
+		text: 'Get Groceries',
+		day: 'June 22nd at 7 pm',
+		reminder: true
+	},
+	{
+		id: 4,
+		text: 'Soccer Practice',
+		day: 'June 26th at 6am',
+		reminder: false
+	}
+]
